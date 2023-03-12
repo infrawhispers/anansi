4efba2b33c88794ceef5a69508130093c5b32365
infrawhispers <infrawhispers@users.noreply.github.com>
2023-03-12 18:19:51 -0400

    [checkpoint] seperate out avx

--- core/lib/base/src/metric.rs
+++ core/lib/base/src/metric.rs
@@ -4,6 +4,9 @@ use crate::metric_aarch;
 #[cfg(all(target_arch = "wasm", target_feature = "simd128",))]
 use crate::metric_wasm;
 
+#[cfg(all(target_arch = "x86_64", target_feature = "fma", target_feature = "avx",))]
+use crate::metric_avx;
+
 /*
     the core metric type that we implement for everything!
     MetricHamming
@@ -31,39 +34,6 @@ unsafe fn _mm256_reduce_add_ps(x: core::arch::x86_64::__m256) -> f32 {
     core::arch::x86_64::_mm_cvtss_f32(x32)
 }
 
-#[cfg(all(target_feature = "fma", target_feature = "avx",))]
-#[inline(always)]
-unsafe fn l2_similarity_avx(arr_a: &[f32], arr_b: &[f32]) -> f32 {
-    let result;
-    let niters = (length / 8) as isize;
-    let mut sum = core::arch::x86_64::_mm256_setzero_ps();
-    let ptr_a = arr_a.as_ptr() as *mut i8;
-    let ptr_a_f = arr_a.as_ptr();
-    let ptr_b = arr_b.as_ptr() as *mut i8;
-    let ptr_b_f = arr_b.as_ptr();
-
-    for j in 0..niters {
-        if j < (niters - 1) {
-            core::arch::x86_64::_mm_prefetch(
-                ptr_a.offset(8 * (j + 1)),
-                core::arch::x86_64::_MM_HINT_T0,
-            );
-            core::arch::x86_64::_mm_prefetch(
-                ptr_b.offset(8 * (j + 1)),
-                core::arch::x86_64::_MM_HINT_T0,
-            );
-        }
-        let a_vec: core::arch::x86_64::__m256 =
-            core::arch::x86_64::_mm256_load_ps(ptr_a_f.offset(8 * j) as *mut f32);
-        let b_vec: core::arch::x86_64::__m256 =
-            core::arch::x86_64::_mm256_load_ps(ptr_b_f.offset(8 * j) as *mut f32);
-        let tmp_vec: core::arch::x86_64::__m256 = core::arch::x86_64::_mm256_sub_ps(a_vec, b_vec);
-        sum = core::arch::x86_64::_mm256_fmadd_ps(tmp_vec, tmp_vec, sum);
-    }
-    result = self::_mm256_reduce_add_ps(sum);
-    result
-}
-
 #[allow(unused_variables)]
 pub(crate) fn l2_similarity(arr_a: &[f32], arr_b: &[f32]) -> f32 {
     return arr_a
@@ -88,7 +58,7 @@ impl Metric for MetricL2 {
         #[cfg(all(target_arch = "x86_64", target_feature = "fma", target_feature = "avx",))]
         {
             if is_x86_feature_detected!("avx") && is_x86_feature_detected!("fma") {
-                return unsafe { l2_similarity_avx(arr_a, arr_b) };
+                return unsafe { metric_avx::l2_similarity_avx(arr_a, arr_b) };
             }
         }
         #[cfg(all(target_arch = "aarch64", target_feature = "neon",))]
@@ -103,40 +73,6 @@ impl Metric for MetricL2 {
         }
         l2_similarity(arr_a, arr_b)
     }
-}
-
-#[cfg(all(target_feature = "avx",))]
-#[inline(always)]
-#[allow(unused_variables)]
-unsafe fn l1_similarity_avx(arr_a: &[f32], arr_b: &[f32]) -> f32 {
-    let result;
-    let niters = (length / 8) as isize;
-    let mut sum = core::arch::x86_64::_mm256_setzero_ps();
-    let ptr_a = arr_a.as_ptr() as *mut i8;
-    let ptr_a_f = arr_a.as_ptr();
-    let ptr_b = arr_b.as_ptr() as *mut i8;
-    let ptr_b_f = arr_b.as_ptr();
-
-    for j in 0..niters {
-        if j < (niters - 1) {
-            core::arch::x86_64::_mm_prefetch(
-                ptr_a.offset(8 * (j + 1)),
-                core::arch::x86_64::_MM_HINT_T0,
-            );
-            core::arch::x86_64::_mm_prefetch(
-                ptr_b.offset(8 * (j + 1)),
-                core::arch::x86_64::_MM_HINT_T0,
-            );
-        }
-        let a_vec: core::arch::x86_64::__m256 =
-            core::arch::x86_64::_mm256_load_ps(ptr_a_f.offset(8 * j) as *mut f32);
-        let b_vec: core::arch::x86_64::__m256 =
-            core::arch::x86_64::_mm256_load_ps(ptr_b_f.offset(8 * j) as *mut f32);
-        let tmp_vec: core::arch::x86_64::__m256 = core::arch::x86_64::_mm256_sub_ps(a_vec, b_vec);
-        sum = core::arch::x86_64::_mm256_add_ps(tmp_vec, sum);
-    }
-    result = self::_mm256_reduce_add_ps(sum);
-    result
 }
 
 #[allow(unused_variables)]
@@ -162,7 +98,7 @@ impl Metric for MetricL1 {
         #[cfg(all(target_arch = "x86_64", target_feature = "fma", target_feature = "avx",))]
         {
             if is_x86_feature_detected!("avx") && is_x86_feature_detected!("fma") {
-                return unsafe { l1_similarity_avx(arr_a, arr_b) };
+                return unsafe { metric_avx::l1_similarity_avx(arr_a, arr_b) };
             }
         }
         #[cfg(all(target_arch = "aarch64", target_feature = "neon",))]
