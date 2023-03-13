0afd57a10af4e610d3d4748f3f4121fba0bf0a79
infrawhispers <infrawhispers@users.noreply.github.com>
2023-03-13 14:03:45 -0400

    [checkpoint] adding hamming similarity

--- core/lib/base/src/metric.rs
+++ core/lib/base/src/metric.rs
@@ -18,23 +18,6 @@ pub trait Metric: Sync + Send {
     fn pre_process(arr_a: &[f32]) -> Option<Vec<f32>>;
 }
 
-#[cfg(all(target_feature = "fma", target_feature = "avx",))]
-#[inline(always)]
-unsafe fn _mm256_reduce_add_ps(x: core::arch::x86_64::__m256) -> f32 {
-    // this is fine since AVX is a superset of SSE - meaning we are guaranted
-    // to have the SSE instructions available to us
-    let x128: core::arch::x86_64::__m128 = core::arch::x86_64::_mm_add_ps(
-        core::arch::x86_64::_mm256_extractf128_ps(x, 1),
-        core::arch::x86_64::_mm256_castps256_ps128(x),
-    );
-    let x64: core::arch::x86_64::__m128 =
-        core::arch::x86_64::_mm_add_ps(x128, core::arch::x86_64::_mm_movehl_ps(x128, x128));
-    let x32: core::arch::x86_64::__m128 =
-        core::arch::x86_64::_mm_add_ss(x64, core::arch::x86_64::_mm_shuffle_ps(x64, x64, 0x55));
-    core::arch::x86_64::_mm_cvtss_f32(x32)
-}
-
-#[allow(unused_variables)]
 pub(crate) fn l2_similarity(arr_a: &[f32], arr_b: &[f32]) -> f32 {
     return arr_a
         .iter()
@@ -53,7 +36,6 @@ impl Metric for MetricL2 {
     }
 
     #[inline(always)]
-    #[allow(unused_variables)]
     fn compare(arr_a: &[f32], arr_b: &[f32]) -> f32 {
         #[cfg(all(target_arch = "x86_64", target_feature = "fma", target_feature = "avx",))]
         {
@@ -75,7 +57,6 @@ impl Metric for MetricL2 {
     }
 }
 
-#[allow(unused_variables)]
 pub(crate) fn l1_similarity(arr_a: &[f32], arr_b: &[f32]) -> f32 {
     return arr_a
         .iter()
@@ -115,29 +96,6 @@ impl Metric for MetricL1 {
     }
 }
 
-// #[cfg(all(target_feature = "neon",))]
-// unsafe fn count_bytes(v: core::arch::aarch64::uint32x4_t) -> core::arch::aarch64::uint32x4_t {
-//     // 0u8, 1u8, 1u8, 2u8
-//     let low_mask: core::arch::aarch64::uint32x4_t = core::arch::aarch64::vdupq_n_u32(0u32);
-//     let lo: core::arch::aarch64::uint32x4_t = core::arch::aarch64::vandq_u32(v, low_mask);
-//     let hi: core::arch::aarch64::uint32x4_t =
-//         core::arch::aarch64::vandq_u32(core::arch::aarch64::vshrq_n_u32(v, 16), low_mask);
-// }
-
-#[allow(unused_variables)]
-fn hamming_similarity(arr_a: &[f32], arr_b: &[f32]) -> f32 {
-    // TODO(lneath) - we use the raw bitwise representations to do hamming
-    // the client needs to be aware of this when creating the vectors and
-    // sending them out to the caller
-    let res: u32 = arr_a
-        .iter()
-        .cloned()
-        .zip(arr_b.iter().cloned())
-        .map(|(a, b)| a.to_bits() & b.to_bits())
-        .sum();
-    res as f32
-}
-
 #[derive(Debug)]
 pub struct Hamming {}
 impl Metric for Hamming {
@@ -147,6 +105,26 @@ impl Metric for Hamming {
     }
     #[inline(always)]
     fn compare(arr_a: &[f32], arr_b: &[f32]) -> f32 {
+        #[cfg(all(target_arch = "aarch64", target_feature = "neon",))]
+        {
+            if std::arch::is_aarch64_feature_detected!("neon") {
+                return unsafe { metric_aarch::hamming_similarity_aarch(arr_a, arr_b) };
+            }
+        }
+
         hamming_similarity(arr_a, arr_b)
     }
+}
+
+pub(crate) fn hamming_similarity(arr_a: &[f32], arr_b: &[f32]) -> f32 {
+    // TODO(lneath) - we use the raw bitwise representations to do hamming
+    // the client needs to be aware of this when creating the vectors and
+    // sending them out to the caller
+    let res: u32 = arr_a
+        .iter()
+        .cloned()
+        .zip(arr_b.iter().cloned())
+        .map(|(a, b)| (a.to_bits() ^ b.to_bits()).count_ones())
+        .sum();
+    res as f32
 }
